021f93cf8c9b21822de0e574cc426a5ac6108d67
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-08-23 10:15:29 -0400

    create basic touchgrass website

---
+++ main.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+async function fetchGrassImage() {
+    const clientId = process.env.UNSPLASH_CLIENT_ID;
+    const response = await fetch(`https://api.unsplash.com/photos/random?query=grass&client_id=${clientId}`);
+    const data = await response.json();
+    const imageUrl = data.urls.small;
+    
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    
+    const container = document.getElementById('image-container');
+    container.innerHTML = '';
+    container.appendChild(imgElement);
+}
+
+fetchGrassImage();
